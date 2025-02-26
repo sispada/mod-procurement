9d6fe4827358f65f409ecb9e87c9c7c4fa02c9e9
monoland <mono.socialmedia@gmail.com>
2025-02-26 07:11:08 +0700

    update

---
+++ src/Events/ProcurementBiodataCreated.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Module\Procurement\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class ProcurementBiodataCreated
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Model $model)
+    {
+        // 
+    }
+}
--- src/Models/ProcurementOfficer.php
+++ src/Models/ProcurementOfficer.php
@@ -132,6 +132,7 @@ class ProcurementOfficer extends Model
             $model->section = $request->section;
             $model->position = $request->position;
             $model->role = 'PPK';
+            
             $parent->officers()->save($model);
 
             DB::connection($model->connection)->commit();
